941bfe7dc63c78c202aea2df52f90ae295fd5fd7
ibrahimalwaki <ibrahimalwaki430@gmail.com>
2026-01-29 16:18:23 -0500

    Iteration 1: add Scheduler server and message protocol

---
+++ PI1/src/Message.java
@@ -0,0 +1,33 @@
+import java.io.Serializable;
+
+public class Message implements Serializable {
+
+    public enum Type {
+        EVENT,          // Fire -> Scheduler (new event)
+        REQUEST_TASK,   // Drone -> Scheduler (ask for work)
+        DISPATCH,       // Scheduler -> Drone (send event to execute)
+        DONE,           // Drone -> Scheduler (finished event)
+        ACK,            // Scheduler -> Fire (confirmation)
+        NO_TASK         // Scheduler -> Drone (nothing available)
+    }
+
+
+    private final Type type;
+    private final Event event;
+    private final String note;
+
+    public Message(Type type, Event event, String note) {
+        this.type = type;
+        this.event = event;
+        this.note = note;
+    }
+
+    public Type getType() { return type; }
+    public Event getEvent() { return event; }
+    public String getNote() { return note; }
+
+    @Override
+    public String toString() {
+        return "Message[type=" + type + ", event=" + event + ", note=" + note + "]";
+    }
+}
