d1ab521052b84194c095ca0a906b4b09eb1580fc
Faraaz Jan <faraaz.jan@gmail.com>
2026-02-09 13:09:43 -0500

    Drones are only created at DroneSubsystem initialization now

--- PI1/src/Drone.java
+++ PI1/src/Drone.java
@@ -1,24 +1,47 @@
 public class Drone implements Runnable {
 
-    private final Event event;
-    private final SchedulerServer scheduler;
+    private Event event;
+    private boolean busy = false;
+    private boolean running = true;
+    private final String droneName;
+    private final DroneSubsystem droneSubsystem;
 
-    public Drone(Event event, SchedulerServer scheduler) {
-        this.event = event;
-        this.scheduler = scheduler;
+    public Drone(DroneSubsystem droneSubsystem, String droneName) {
+        this.droneSubsystem = droneSubsystem;
+        this.event = null;
+        this.droneName = droneName;
+    }
+
+    public boolean isBusy() {
+        return this.busy;
     }
 
     @Override
     public void run() {
-        System.out.println("[Drone] Servicing fire at zone " + event.getZoneID());
+        while(running) {
+            if(isBusy() && event != null) {
+                System.out.println("["+droneName+"] Servicing fire at zone " + event.getZoneID());
+                try {
+                    Thread.sleep(2000); //place holder for drone time calculation later
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
 
-        try {
-            Thread.sleep(2000); //place holder for drone time calculation later
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+                droneSubsystem.reportDone(event);
+                System.out.println("["+droneName+"] Completed fire at zone " + event.getZoneID());
+                this.event = null;
+                this.busy = false;
+            } else {
+                try {
+                    System.out.println("["+droneName+"] Waiting for task...");
+                    event = droneSubsystem.requestTask(); // blocks until work
+                    this.busy = true;
+                } catch (InterruptedException e) {
+                    System.out.println("["+droneName+"] Interrupted, shutting down");
+                    running = false;
+                    Thread.currentThread().interrupt();
+                }
+            }
         }
-
-        scheduler.reportDone(event);
-        System.out.println("[Drone] Completed fire at zone " + event.getZoneID());
     }
 }
